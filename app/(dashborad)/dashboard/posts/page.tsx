70c85283eb1a973e9e920253e2859ca0c9931838
renandcisuporte <renan@dcisuporte.com.br>
2025-01-28 16:52:18 -0300

    ajustar image galeria do adm

--- app/(dashborad)/dashboard/posts/page.tsx
+++ app/(dashborad)/dashboard/posts/page.tsx
@@ -76,13 +76,14 @@ export default async function Page({ searchParams }: Props) {
         <TableBody>
           {posts?.map((item) => (
             <TableRow key={item.id}>
-              <TableCell className="w-[1%]">
+              <TableCell className="w-[175px]">
                 <Image
                   src={item.coverImage!}
                   alt={item.title}
                   loading="lazy"
-                  width={105}
-                  height={105}
+                  width={175}
+                  height={175}
+                  quality={80}
                 />
               </TableCell>
               <TableCell className="w-[1%]">
