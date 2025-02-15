845aaccc7fdb186b385cb587aeb046c5ed6fbc05
renandcisuporte <renan@dcisuporte.com.br>
2025-02-15 11:01:30 -0300

    adicionar download na galeria/aumentar foto galeria

--- app/(dashborad)/dashboard/posts/[form]/upload/page.tsx
+++ app/(dashborad)/dashboard/posts/[form]/upload/page.tsx
@@ -1,10 +1,15 @@
 import { postAction } from '@/core/main/config/dependencies'
+import { Metadata } from 'next'
 import { PageClient } from './page-client'
 
 type Props = {
   params: {
     form: string
   }
+}
+
+export const metadata: Metadata = {
+  title: 'Upload Postagem/Eventos - Itachique'
 }
 
 export default async function Page({ params }: Props) {
---
+++ app/api/download/route.ts
@@ -0,0 +1,35 @@
+import { readFile } from 'fs/promises'
+import { NextRequest, NextResponse } from 'next/server'
+import path from 'path'
+
+export async function GET(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url)
+    const image = searchParams.get('image')
+    const ext = image?.split('.').pop()
+
+    if (!image && ['png', 'jpg', 'jpeg'].includes(ext!)) {
+      throw new Error('Arquivo não suportado')
+    }
+
+    // process.cwd() is the root of the Next.js app
+    const buffer = await readFile(path.join(process.cwd(), 'public', image!))
+
+    //  set the headers to tell the browser to download the file
+    const headers = new Headers()
+
+    // remember to change the filename `test.pdf` to whatever you want the downloaded file called
+    headers.append('Content-Disposition', 'attachment; filename=' + image)
+    headers.append('Content-Type', 'application/jpeg')
+
+    return new Response(buffer, {
+      headers
+    })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json(
+      { message: 'Erro ao processar upload.' },
+      { status: 400 }
+    )
+  }
+}
