db9a6e6bb76b05808b695138ce1210c395704fc3
renandcisuporte <renan@dcisuporte.com.br>
2025-07-24 14:25:11 -0300

    chore: ajustes para não deixar site off

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 set -e
 cd /home/itachiqu/site.itachique.com.br/ || true
 
-echo '# Stop project'
-pm2 stop ecosystem.config.js
-sleep 1
+# echo '# Stop project'
+# pm2 stop ecosystem.config.js
+# sleep 1
 
 echo "# Installing dependencies"
 npm install
@@ -24,6 +24,6 @@ npm run build
 sleep 1
 
 echo "# Running server PM2"
-pm2 start ecosystem.config.js --env production
+pm2 restart ecosystem.config.js --env production
 pm2 startup
 pm2 save
